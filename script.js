717bacba6d371764cebae764fb9dfd3b4c7daa0a
yacinekcl <yacine7kecili@gmail.com>
2025-04-30 15:16:12 +0100

    update - insim

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ document.addEventListener("DOMContentLoaded", function () {
     if (input3) document.getElementById(`name3-${section}`).textContent = input3;
   }
   
+
+  // scroll to top
+  // Get the button
+var mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' }); 
+}
