a2a2968f94fd07a45a863a6691a20ddad5371a40
yacinekcl <yacine7kecili@gmail.com>
2025-04-30 11:52:11 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mobile hamburger menu
 document.addEventListener("DOMContentLoaded", function () {
     const toggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('nav-menu');
@@ -7,6 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  // scroll to presentation 
+
   function scrollToMenu() {
     document.getElementById('presentation').scrollIntoView({ behavior: 'smooth' });
   }
+
+  // podium input
+  function updateNames(section) {
+    const input1 = document.getElementById(`input1-${section}`).value.trim();
+    const input2 = document.getElementById(`input2-${section}`).value.trim();
+    const input3 = document.getElementById(`input3-${section}`).value.trim();
+  
+    if (input1) document.getElementById(`name1-${section}`).textContent = input1;
+    if (input2) document.getElementById(`name2-${section}`).textContent = input2;
+    if (input3) document.getElementById(`name3-${section}`).textContent = input3;
+  }
+  
